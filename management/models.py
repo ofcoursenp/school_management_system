2600dba9d0e00f566e5c589a3f885538e1794489
ofcoursenp <nexusalt33@gmail.com>
2023-03-18 15:45:10 +0545

    Added basic models v2

--- management/models.py
+++ management/models.py
@@ -10,23 +10,18 @@ class BaseUser(models.Model):
     is_verified = models.BooleanField(default=False)
     bio = models.CharField(max_length=300)
     profilePic = models.ImageField(null=True,blank=True,upload_to='images/profile/')
+    school = models.CharField(max_length=100,default='Nava Jyoti')
+    grade = models.IntegerField(validators=[MaxValueValidator(12)])
 
     def __str__(self):
         return str(self.user)
 
-class user_data(models.Model):
-    school = models.CharField(max_length=100,default='Nava Jyoti')
-    grade = models.IntegerField(validators=[MaxValueValidator(12)])
-    
-    def __str__(self):
-        return str(self.school)
 
-
-class Post(models.Model):
+class Certificates(models.Model):
     image = models.ImageField(null=True,blank=True,upload_to='post/images')
     created_on = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.user)
-
+    
